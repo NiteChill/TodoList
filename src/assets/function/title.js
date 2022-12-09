fd068669ca0b9844b17b0d1f37f01e75c2748811
NiteChill <puissantachille@gmail.com>
2022-12-09 18:38:40 +0100

    title 70% done

--- src/assets/function/title.js
+++ src/assets/function/title.js
@@ -6,6 +6,10 @@ function title() {
   titleContainer.classList.add('titleContainer');
   const titleSave = document.createElement('i');
   titleSave.className = 'fa-regular fa-floppy-disk fa-2x iconTextSave';
+  const inputTextContainer = document.createElement('div');
+  inputTextContainer.classList.add('inputTextContainer');
+  const inputContainer = document.createElement('div');
+  inputContainer.classList.add('inputContainer');
   const titleText = document.createElement('div');
   titleText.classList.add('titleText');
   titleText.textContent= buildArray[0].title;
@@ -13,10 +17,28 @@ function title() {
   titleInput.setAttribute('type', 'text');
   titleInput.classList.add('titleInput');
   const titleEdit = document.createElement('i');
-  titleEdit.className= 'fa-regular fa-pen-to-square fa-2x margin-bot iconTextEdit';
-  titleContainer.append( titleSave, titleText, titleInput, titleEdit );
+  titleEdit.className= 'fa-regular fa-pen-to-square fa-2x iconTextEdit';
+  inputContainer.append( titleInput )
+  inputTextContainer.append( titleText, inputContainer );
+  titleContainer.append( titleSave, inputTextContainer, titleEdit );
+  let editTF = false;
   titleEdit.addEventListener('click', () => {
-    
+    if ( editTF ) {
+      titleEdit.classList.replace('fa-xmark', 'fa-pen-to-square');
+      titleEdit.classList.replace('fa-solid', 'fa-regular');
+      titleEdit.style.marginBot = '0';
+      titleInput.style.width = '0';
+      titleText.style.display = 'flex';
+      editTF = false;
+    } else {
+      titleEdit.classList.replace('fa-pen-to-square', 'fa-xmark');
+      titleEdit.classList.replace('fa-regular', 'fa-solid');
+      titleEdit.style.marginBot = '0';
+      titleInput.style.width = '100%';
+      titleText.style.display = 'none';
+      editTF = true;
+    }
+    // titleInput.style.width = titleText.getBoundingClientRect().width + "px";
   })
   app.append( titleContainer );
 }
